6d6804de598d6d8d487418ba0992626efbce06e5
Cirelli <cirelli94@gmail.com>
2017-01-11 16:22:32 +0100

    creato ana-creator che crea lista con anagrammi

---
+++ ana-creator.py
@@ -0,0 +1,18 @@
+words = open ('words.txt')
+dict={}
+for word in words:
+	word=word.strip()
+	if (len(word)>2 and len(word)<8):
+		key = ''.join(sorted(word))
+		if key in dict:
+			#print(key)
+			dict[key].append(word)
+		else :
+			dict[key]= [word]
+
+words.close()
+anadict = [' '.join([key]+value) for key, value in dict.items()]
+anadict.sort()
+f = open('anadict.txt','w')
+f.write('\n'.join(anadict))
+f.close()
