4ce74883fd930628a69a76b6cbc909304aa7a725
Sourcery AI <>
2022-12-05 02:19:25 +0000

    'Refactored by Sourcery'

--- anacreator.py
+++ anacreator.py
@@ -1,16 +1,14 @@
-words = open ('words.txt')
-dict={}
-for word in words:
-	word=word.strip()
-	if (len(word)>2 and len(word)<8):
-		key = ''.join(sorted(word))
-		if key in dict:
-			dict[key].append(word)
-		else :
-			dict[key]= [word]
-words.close()
+with open ('words.txt') as words:
+	dict={}
+	for word in words:
+		word=word.strip()
+		if (len(word)>2 and len(word)<8):
+			key = ''.join(sorted(word))
+			if key in dict:
+				dict[key].append(word)
+			else :
+				dict[key]= [word]
 anadict = [' '.join([key]+value) for key, value in dict.items()]
 anadict.sort()
-f = open('anadict.txt','w')
-f.write('\n'.join(anadict))
-f.close()
+with open('anadict.txt','w') as f:
+	f.write('\n'.join(anadict))
